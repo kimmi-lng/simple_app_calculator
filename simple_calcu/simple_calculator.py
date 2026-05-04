06d4be90fab73f7d58e2ba18f8a9c905c7257416
Kim Russel <kimrussel0718@gmail.com>
2026-05-04 17:31:04 +0800

    added the parent and child class, under parents class i added some fuctions there i also added functions for the operations so tha we can call them later at child class, to make it clean ill be adding additionals like clearing the terminal for clean looks and header for good tect based ui.

--- simple_calcu/simple_calculator.py
+++ simple_calcu/simple_calculator.py
@@ -1,1 +1,27 @@
-ECHO is on.
+# Activity 6: Exception Handling
+
+# Instructions:
+# - Do the programming exercises in module 3 page 21.
+# - Upload your code in a new GitHub repository.
+# - Git commit on every milestone, part of your grade is based on your git commit. 
+# - Code should be OOP and have inheritance implementation
+# - Feel free to add anything that will make your program maangas
+# - Create a demo vid
+# - Send me the link of your GitHub repo and demo before May 9
+
+#parent class
+class BasicCalculator:
+
+#for additional
+    def clear(self): #clear the screen import os?
+    def header(self): #display the header
+#operations
+    def add(self, num1, num2):
+    def subtract(self, num1, num2):
+    def multiply(self, num1, num2):
+    def divide(self, num1, num2):
+
+#child class
+class calculation(BasicCalculator):
+    def __init__(self):
+        super().__init__() 
